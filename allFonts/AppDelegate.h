270ade5244d8699a5a56154edb86876cbc350373
lzz <arloou@gmail.com>
2015-01-19 16:05:25 +0800

    Initial Commit

---
+++ allFonts/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  allFonts
+//
+//  Created by ucs on 15/1/19.
+//  Copyright (c) 2015年 ucs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
